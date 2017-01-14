86067eb5262cb8aea8715e96c644dcc3388776de
Shen Chang-Shao <stationaryfront211@gmail.com>
2017-01-14 16:31:08 +0800

    detection control

--- Detection_controller.sv
+++ Detection_controller.sv
@@ -12,14 +12,31 @@ module Detection_controller (
 
 	always_comb begin
 		
+		case (state_r)
+			WHITE: begin 
+				start_detect = 0;
+				finish_detect = 1;
+				if(vga_b > 8'b10101111 && vga_g > 8'b10101111 && vga_r > 8'b10101111) begin
+					state_w = BLACK;		
+				end
+			end
+			BLACK: begin
+				start_detect = 1;
+				finish_detect = 0;
+				if(vga_b < 8'b00011111 && vga_g < 8'b00011111 && vga_r < 8'b00011111) begin
+					state_w = WHITE;
+				end
+			end
+			default : /* default */;
+		endcase
 	end
 	
 
 	always_ff @(posedge clk or negedge rst) begin 
 		if(~rst) begin
-			 <= 0;
+			state_r <= WHITE;
 		end else begin
-			 <= ;
+			state_r <= state_w;
 		end
 	end
 endmodule
